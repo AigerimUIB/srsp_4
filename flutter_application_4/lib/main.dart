f5f13ff02406032fde7b39ee427aaa1f241c5f82
Alashbayeva Aigerim <2111268@uib.kz>
2024-02-23 11:26:39 +0600

    first commit

---
+++ flutter_application_4/lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Navigation Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: FirstScreen(),
+    );
+  }
+}
+
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => SecondScreen(),
+              ),
+            ).then((value) {
+              // Обработка возвращаемых данных из второго экрана
+              print('Returned data from Second Screen: $value');
+            });
+          },
+          child: Text('Перейти к следующему экрану'),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Возврат данных на первый экран
+            Navigator.pop(context, 'Data from Second Screen');
+          },
+          child: Text('Вернуться'),
+        ),
+      ),
+    );
+  }
+}
